9339a66d52168d54a330b760d49699b1b30a1634
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-11-15 17:09:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,23 @@
-import streamlit as st
+# streamlit_app.py
 
-st.write("Amazon S3 Connection Example")
+import streamlit as st
+import s3fs
+import os
+
+# Create connection object.
+# `anon=False` means not anonymous, i.e. it uses access keys to pull data.
+fs = s3fs.S3FileSystem(anon=False)
+
+# Retrieve file contents.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def read_file(filename):
+    with fs.open(filename) as f:
+        return f.read().decode("utf-8")
+
+content = read_file("testbucket-jrieke/myfile.csv")
+
+# Print results.
+for line in content.strip().split("\n"):
+    name, pet = line.split(",")
+    st.write(f"{name} has a :{pet}:")
