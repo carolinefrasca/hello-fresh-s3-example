50ec70b97284fd2cb3a1aeed4f3d1130288821ed
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-11-15 21:02:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,26 @@
 import streamlit as st
 import s3fs
-import pandas as pd
+import os
 
 st.title("Streamlit x HelloFresh")
 st.header("AWS S3 Demo")
          
-# Create connection object
+# Create connection object.
 # `anon=False` means not anonymous, i.e. it uses access keys to pull data.
 fs = s3fs.S3FileSystem(anon=False)
 
-# Retrieve file contents 
+# Retrieve file contents. 
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
 def read_file(filename):
     with fs.open(filename) as f:
-        #df = pd.read_csv(f)
         return f.read().decode("utf-8")
 
 content = read_file("hellofreshexample/HelloFreshExample - SampleData.csv")
 
-# Print results
+# Print results.
 st.subheader("Read data from CSV file:")
 for line in content.strip().split("\n")[1:]:
     first_name, last_name, dietary_restrictions, fave_ingredient = line.split(",")
     st.write(f"{first_name} {last_name}'s favorite ingredient is {fave_ingredient}.")
+#     st.write(f"First name: {first_name}; Last name: {last_name}")
