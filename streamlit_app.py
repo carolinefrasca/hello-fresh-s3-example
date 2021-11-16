30f624335ca8b6932ca67141c0db30745ed3ca05
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-11-15 20:14:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ def read_file(filename):
     with fs.open(filename) as f:
         return f.read().decode("utf-8")
 
-content = read_file("hellofreshexample/HelloFreshExample+-+SampleData.csv")
+content = read_file("hellofreshexample/HelloFreshExample - SampleData.csv")
 
 # Print results.
 for line in content.strip().split("\n"):
